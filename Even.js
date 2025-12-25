f11faa5df51c3d69b17037b8bbcbabd639a5e9ac
Fida-ur-Rahman <fidasaba15@gmail.com>
2025-12-26 00:21:21 +0500

    JavaScript Assignment no 5

---
+++ Even.js
@@ -0,0 +1,13 @@
+// Even or Odd
+let num = 1;
+if (num % 2 == 0){
+    console.log(num , "is Even Number")
+}
+else{
+    console.log(num , "is Odd Number")
+}
+
+// Turnary operators
+let age = 25;
+let Result = age>=18 ? "adult" : "Not adult";
+console.log(Result)
